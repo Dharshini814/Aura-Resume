ad3f5b0e6c049d1722632a30e8667975cb67427a
Dharshini814 <dharshinij812@gmail.com>
2026-05-31 20:33:15 +0530

    Update passport.js

--- Aura Resume/Backend/config/passport.js
+++ Aura Resume/Backend/config/passport.js
@@ -1,46 +1,59 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+/* SESSION */
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 /* GOOGLE */
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
       callbackURL:
         "https://aura-resume-dujg.onrender.com/api/auth/google/callback",
     },
-
-    async (
-      accessToken,
-      refreshToken,
-      profile,
-      done
-    ) => {
+    async (accessToken, refreshToken, profile, done) => {
       try {
         // Check if Google account already exists
         let user = await User.findOne({
           googleId: profile.id,
         });
 
-        if (!user) {
-          // Check if user already exists with same email
-          user = await User.findOne({
-            email: profile.emails[0].value,
-          });
-
-          if (user) {
-            // Link Google account to existing user
-            user.googleId = profile.id;
-            await user.save();
-          } else {
-            // Create new user
-            user = await User.create({
-              googleId: profile.id,
-              name: profile.displayName,
-              email: profile.emails[0].value,
-            });
-          }
+        if (user) {
+          return done(null, user);
         }
+
+        // Check if email already exists
+        user = await User.findOne({
+          email: profile.emails[0].value,
+        });
+
+        if (user) {
+          user.googleId = profile.id;
+          await user.save();
+
+          return done(null, user);
+        }
+
+        // Create new user
+        user = await User.create({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+        });
 
         return done(null, user);
       } catch (error) {
@@ -50,3 +63,5 @@ passport.use(
     }
   )
 );
+
+module.exports = passport;
