eefdd7f7275dee960462833989eb8082c0affd43
Dharshini814 <dharshinij812@gmail.com>
2026-05-31 20:28:17 +0530

    Update passport.js

--- Aura Resume/Backend/config/passport.js
+++ Aura Resume/Backend/config/passport.js
@@ -1,35 +1,13 @@
-const passport = require("passport");
-
-const GoogleStrategy =
-  require("passport-google-oauth20").Strategy;
-
-/*const FacebookStrategy =
-  require("passport-facebook").Strategy;*/
-
-const User = require("../models/User");
-
-/* SESSION */
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-
-  done(null, user);
-});
-
 /* GOOGLE */
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        process.env.GOOGLE_CLIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
 
-      clientSecret:
-        process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-     callbackURL: "https://aura-resume-dujg.onrender.com/api/auth/google/callback",
+      callbackURL:
+        "https://aura-resume-dujg.onrender.com/api/auth/google/callback",
     },
 
     async (
@@ -39,75 +17,36 @@ passport.use(
       done
     ) => {
       try {
+        // Check if Google account already exists
         let user = await User.findOne({
           googleId: profile.id,
         });
 
         if (!user) {
-          user = await User.create({
-            googleId: profile.id,
-            name: profile.displayName,
+          // Check if user already exists with same email
+          user = await User.findOne({
             email: profile.emails[0].value,
           });
+
+          if (user) {
+            // Link Google account to existing user
+            user.googleId = profile.id;
+            await user.save();
+          } else {
+            // Create new user
+            user = await User.create({
+              googleId: profile.id,
+              name: profile.displayName,
+              email: profile.emails[0].value,
+            });
+          }
         }
 
-        done(null, user);
+        return done(null, user);
       } catch (error) {
-        done(error, null);
+        console.error("Google Auth Error:", error);
+        return done(error, null);
       }
     }
   )
 );
-
-/* FACEBOOK 
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID:
-        process.env.FACEBOOK_APP_ID,
-
-      clientSecret:
-        process.env.FACEBOOK_APP_SECRET,
-
-      callbackURL: "http://localhost:5000/api/auth/facebook/callback",
-
-      profileFields: [
-        "id",
-        "emails",
-        "name",
-      ],
-    },
-
-    async (
-      accessToken,
-      refreshToken,
-      profile,
-      done
-    ) => {
-      try {
-        let user = await User.findOne({
-          facebookId: profile.id,
-        });
-
-        if (!user) {
-          user = await User.create({
-            facebookId: profile.id,
-
-            name:
-              profile.name.givenName +
-              " " +
-              profile.name.familyName,
-
-            email:
-              profile.emails?.[0]?.value ||
-              `${profile.id}@facebook.com`,
-          });
-        }
-
-        done(null, user);
-      } catch (error) {
-        done(error, null);
-      }
-    }
-  )
-);*/
